50dfadff5c6036e38b697005fbcfa87e24da7336
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-09-24 08:35:35 +0900

    Mortality data gather

---
+++ Lectures/Lecture5/WDMortalitGDP.R
@@ -0,0 +1,29 @@
+##################
+# Gather World Bank Data on Mortality Rate & GDP per capita for 2009
+# Christopher Gandrud
+# Updated 24 September 2012
+##################
+
+#### Data to gather ####
+# Mortality rate, infant (per 1,000 live births): SP.DYN.IMRT.IN
+# GDP per capita (current US$): NY.GDP.PCAP.CD
+
+# Load package
+library(WDI)
+library(countrycode)
+library(reshape)
+
+# Download data
+MortalityGDP <- WDI(indicator = c("NY.GDP.PCAP.CD", "SP.DYN.IMRT.IN"), start = 2009, end = 2009)
+
+# Keep only countries
+MortalityGDP$IMFCode <- countrycode(MortalityGDP$country, origin = "country.name", destination = "imf")
+
+MortalityGDP <- subset(MortalityGDP, !is.na(IMFCode))
+
+# Rename the main variables
+MortalityGDP <- rename(MortalityGDP, c(SP.DYN.IMRT.IN = "InfantMortality"))
+MortalityGDP <- rename(MortalityGDP, c(NY.GDP.PCAP.CD = "GDPperCapita"))
+
+# Keep country GDPperCapita and InfantMortality variables
+MortalityGDP <- MortalityGDP[, c("country", "GDPperCapita", "InfantMortality")]
